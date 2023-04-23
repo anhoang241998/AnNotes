b87af53bc7c3d16405c20bfbf49f3e57d2abf5e4
Nguyen Hoang An <an.nguyenhoang@nashtechglobal.com>
2023-04-23 23:56:57 +0700

    convert shared flow to channel

--- app/src/main/java/com/annguyenhoang/annotes/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/annguyenhoang/annotes/presentation/login/LoginViewModel.kt
@@ -4,9 +4,10 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.annguyenhoang.annotes.data.AuthRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -15,12 +16,12 @@ class LoginViewModel @Inject constructor(
     private val noteRepository: AuthRepository
 ) : ViewModel() {
 
-    private val _uiState: MutableSharedFlow<LoginUiState?> = MutableSharedFlow()
-    val uiState: SharedFlow<LoginUiState?> = _uiState
+    private val _uiState: Channel<LoginUiState?> = Channel()
+    val uiState: Flow<LoginUiState?> = _uiState.receiveAsFlow()
 
     fun login(username: String) = viewModelScope.launch {
-        _uiState.emit(LoginUiState.Loading)
-        noteRepository.login(username).collectLatest(_uiState::emit)
+        _uiState.send(LoginUiState.Loading)
+        noteRepository.login(username).collectLatest(_uiState::send)
     }
 
 }
